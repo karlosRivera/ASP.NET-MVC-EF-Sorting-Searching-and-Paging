b52e57067dc0945b45e15d46885da9b2453be714
tridipkolkata <tridip2000@gmail.com>
2018-02-12 19:54:15 +0530

    tabular data post from webgrid solved

--- MVCCRUDPageList/Controllers/WebGridEditableController.cs
+++ MVCCRUDPageList/Controllers/WebGridEditableController.cs
@@ -6,21 +6,38 @@ using System.Web.Mvc;
 
 namespace MVCCRUDPageList.Controllers
 {
+
     public class WebGridEditableController : Controller
     {
         // GET: WebGridEditable
         public ActionResult Index()
         {
-            List<UserModel> users = UserModel.getUsers();
-            return View(users);
+            var usermodel = new UserVM
+            {
+                UserList = new List<UserModel>
+                {
+                    new UserModel (){ ID=1, FirstName="Anubhav", LastName="Chaudhary" },   
+                    new UserModel (){ ID=2, FirstName="Mohit", LastName="Singh" },  
+                    new UserModel (){ ID=3, FirstName="Sonu", LastName="Garg" },  
+                    new UserModel (){ ID=4, FirstName="Shalini", LastName="Goel" },  
+                    new UserModel (){ ID=5, FirstName="James", LastName="Bond" }
+                }
+            };
+
+            return View(usermodel);
         }
 
         // GET: WebGridEditable
         [HttpPost]
-        public ActionResult Index(List<UserModel> oUserModel)
+        public ActionResult Index(UserVM oUserVM)
         {
-            return View(oUserModel);
+            return View(oUserVM);
         }
+    }
+
+    public class UserVM
+    {
+        public List<UserModel> UserList { get; set; }
     }
 
     public class UserModel
@@ -28,18 +45,5 @@ namespace MVCCRUDPageList.Controllers
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
-        public static List<UserModel> getUsers()
-        {
-            List<UserModel> users = new List<UserModel>()  
-            {  
-                new UserModel (){ ID=1, FirstName="Anubhav", LastName="Chaudhary" },   
-                new UserModel (){ ID=2, FirstName="Mohit", LastName="Singh" },  
-                new UserModel (){ ID=3, FirstName="Sonu", LastName="Garg" },  
-                new UserModel (){ ID=4, FirstName="Shalini", LastName="Goel" },  
-                new UserModel (){ ID=5, FirstName="James", LastName="Bond" },  
-            };
-            return users;
-        }
     }  
 }
---
+++ MVCCRUDPageList/Controllers/WebGridTestController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCRUDPageList.Controllers
+{
+    public class WebGridTestController : Controller
+    {
+        // GET: WebGridTest
+        public ActionResult Index()
+        {
+            var objstudentmodel = StudentRecord();
+            return View(objstudentmodel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(StudentModel objstudentmodel)
+        {
+            //var stusection = GetSectionList();
+            //foreach (var item in objstudentmodel.StudentList)
+            //{
+            //    var objsection = new SelectList(stusection, item.StudentSection);
+            //    item.SectionModel = objsection;
+            //}
+            return View(objstudentmodel);
+        }
+
+        public StudentModel StudentRecord()
+        {
+            var objstudentmodel = new StudentModel
+            {
+                StudentList = new List<Student>
+                {
+                    new Student
+                    {
+                        Name = "Name1",
+                        ClassOfStudent = "12th",
+                        Address = "Address1"
+                    },
+                    new Student
+                    {
+                        Name = "Name2",
+                        ClassOfStudent = "5th",
+                        Address = "Address2"
+                    },
+                    new Student
+                    {
+                        Name = "Name3",
+                        ClassOfStudent = "10th",
+                        Address = "Address3"
+                    },
+                    new Student
+                    {
+                        Name = "Name4",
+                        ClassOfStudent = "1st",
+                        Address = "Address4"
+                    }
+                }
+            };
+            return objstudentmodel;
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string ClassOfStudent { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class StudentModel
+    {
+        public List<Student> StudentList { get; set; }
+    }
+}
