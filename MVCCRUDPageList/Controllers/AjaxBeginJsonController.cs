0b83b191b6042608786a84193ff9f48c2b84fbdb
tridipkolkata <tridip2000@gmail.com>
2018-02-13 14:11:39 +0530

    AjaxBeginForm Newsletter

---
+++ MVCCRUDPageList/Controllers/AjaxBeginJsonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCRUDPageList.other.Controllers
+{
+    public class AjaxBeginJsonController : Controller
+    {
+        // GET: AjaxBeginJson
+        public ActionResult Index()
+        {
+            Customer oCustomer = new Customer();
+            return View(oCustomer);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Customer oCustomer)
+        {
+            return View(oCustomer);
+        }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
